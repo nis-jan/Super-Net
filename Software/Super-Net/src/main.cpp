4aa0acf377f2ed79883c97d646eedcd233142489
jannis <s0573073@htw-berlin.de>
2022-10-18 12:53:08 +0200

    added all the files

--- Software/Super-Net/src/main.cpp
+++ Software/Super-Net/src/main.cpp
@@ -10,12 +10,22 @@
 //entspricht IRQ
 #define LoRa_DIO0 26
 
+bool test_routing = false;
+Packet * p;
+
+void Button_ISR(){
+  test_routing = true;
+  Log::Instance()->Log_msg("pressed");
+  digitalWrite(25, !digitalRead(25));
+}
 
 
 /// <summary>
 /// Setup routine, runs once and initializes the node
 /// </summary>
 void setup() {
+  pinMode(0, INPUT);
+  attachInterrupt(digitalPinToInterrupt(0), Button_ISR, RISING);
   pinMode(25, OUTPUT);
   
   Serial.begin(9600);
@@ -33,7 +43,10 @@ void setup() {
 
 
 int packetsize;
-
+int ref;
+String msgsend=  "";
+char * msgptr;
+std::vector<int> route = {2,1,2000};
 /// <summary>
 /// loop runs continuously and looks for incoming data via Usb serial or LoRa
 /// </summary>
@@ -41,7 +54,16 @@ void loop() {
   if(Serial.available()){
     SerialHandler::InterpretPacket();
   }
-
+  if(test_routing){
+    Log::Instance()->Log_msg("sending msg");
+    p = new Packet();
+    ref = Packet::create_reference();
+    msgsend = "hiii ref:" + String(ref);
+    msgptr = (char*)msgsend.c_str();
+    Packet::create_msg(p, route[route.size()-1], ref, route, msgsend.length(), msgptr);
+    sendrcv::sendpacket(p, true);
+    test_routing = false;
+  }
 
   packetsize = LoRa.parsePacket();
   try{
